1bf4b83156a465fe4d2292ddd01f7dce473f2ae8
Daniel Williams <daniellanierwilliams@gmail.com>
2020-01-15 17:43:16 -0600

    added menu icons, added burger-menu library for side profile menu slide, began creating event handlers for menu

--- src/components/nav/navcontainer.js
+++ src/components/nav/navcontainer.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookOpen, faTags, faHistory, faShoppingBag, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import { Container, Row, Col } from "shards-react";
+import sideBar from "../profileSlide/profileSlide.js"
 import "./Navbar.css";
 
 
@@ -12,11 +15,26 @@ export default class NavContainer extends React.Component {
   
 
         <Row>
-          <Col>Deals</Col>
-          <Col>Menu</Col>
-          <Col>Bag</Col>
-          <Col>Previous</Col>
-          <Col>Profile</Col>
+          <Col>
+          <FontAwesomeIcon icon={faTags}/>
+          <h6>Deals</h6>
+          </Col>
+          <Col>
+          <FontAwesomeIcon icon={faBookOpen}/>
+          <h6>Menu</h6>
+          </Col>
+          <Col>
+          <FontAwesomeIcon icon={faShoppingBag}/>
+          <h6>Bag</h6>
+          </Col>
+          <Col>
+          <FontAwesomeIcon icon={faHistory}/>
+          <h6>Previous</h6>
+          </Col>
+          <Col>
+          <FontAwesomeIcon icon={faUserCircle}/>
+          <h6>Profile</h6>
+          </Col>
         </Row>
 
       </Container>
