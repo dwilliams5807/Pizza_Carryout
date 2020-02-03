04a033c379bae369beafe355d6e1363a79bc4424
Daniel Williams <daniellanierwilliams@gmail.com>
2020-02-03 17:17:32 -0600

    updated menu component to render each item from the cart reducer item array into a list

--- src/components/pages/Menu/Menu.js
+++ src/components/pages/Menu/Menu.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux'
+import {
+  Card,
+  CardTitle,
+  CardImg,
+  CardSubtitle,
+  Container,
+  Row,
+  Col,
+  CardBody
+} from "shards-react";
 
 
-function Menu () {
-  return (
-  <div>
-    <p>Menu goes here</p>
-  </div>
-  );
+class Menu extends Component {
+
+  render() {
+    let itemList = this.props.items.map(item=>{
+      return (
+        <Card key={item.id}>
+          <CardBody>
+           <CardTitle>{item.title}</CardTitle>
+           <CardImg src={item.img} alt={item.title} />
+           <CardSubtitle>{item.price}</CardSubtitle>
+         </CardBody>
+       </Card>
+      )
+    })
+    return ( 
+      <Container> 
+        <Row> 
+          <Col>
+          {itemList}
+          </Col>
+        </Row>
+      </Container> 
+       );
+  }
+
+   
 }
-
-export default Menu;
+const mapStateToProps = (state)=>{
+  return {
+    items: state.items
+  }
+}
+export default connect(mapStateToProps)(Menu);
