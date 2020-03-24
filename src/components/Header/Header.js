c28f2d10c6eb8f0252342b8e316d08d31ee6fc65
Daniel Williams <daniellanierwilliams@gmail.com>
2020-03-24 10:59:35 -0500

    added location indicator and link to header and removed dropdown

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,81 +29,17 @@ import { Link } from 'react-router-dom';
 
 
 class Header extends Component{
-  constructor(props) {
-    super(props);
-
-    this.toggleDropdown = this.toggleDropdown.bind(this);
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-
-    this.state = {
-      dropdownOpen: false,
-      collapseOpen: false,
-      
-    };
-    this.changeLocation = this.changeLocation.bind(this);
-  }
-  changeLocation(location) {
-    this.setState({
-      ...this.state,
-      ...{
-        selectedLocation: location
-      }
-    });
-    console.log(this.props.selectedLocation)
-
-
-  }
-  
-  toggleDropdown() {
-    this.setState({
-      ...this.state,
-      ...{
-        dropdownOpen: !this.state.dropdownOpen
-      }
-    });
-  }
-
-  toggleNavbar() {
-    this.setState({
-      ...this.state,
-      ...{
-        collapseOpen: !this.state.collapseOpen
-      }
-    });
-  }
 
   render(){
       
      return(
       <Navbar className="navbar" type="dark" expand="md">
       <NavbarBrand className="" href="#">
-       <div>{logo}</div> 
+       Chuck E. Carryout
       </NavbarBrand>
-      <NavbarToggler onClick={this.toggleNavbar} />
-
-        <Collapse open={this.state.collapseOpen} navbar>
-      
-          <Nav navbar >
-
-            <Dropdown
-              open={this.state.dropdownOpen}
-              toggle={this.toggleDropdown}
-            >
-              <DropdownToggle nav caret>
-                {this.state.selectedLocation}
-              </DropdownToggle>
-              <DropdownMenu>
-                
-                <DropdownItem name="location" value={"South Austin"} onClick={() => {this.changeLocation("South Austin");}} >South Austin</DropdownItem>
-                <DropdownItem name="location" value={"Mopac"} onClick={() => {this.changeLocation("Mopac");}} >Mopac</DropdownItem>
-                <DropdownItem name="location" value={"Round Rock"} onClick={() => {this.changeLocation("Round Rock");}} >Round Rock</DropdownItem>
-              
-              </DropdownMenu>
-            </Dropdown>
-          </Nav>
-       
-
-      </Collapse>
+       <NavLink href='/'> 
+         {this.props.location}
+       </NavLink>
      </Navbar>
      )
   }
